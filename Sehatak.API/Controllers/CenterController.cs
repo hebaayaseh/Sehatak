4a35577c36df3483b4b4bc4daa151048cd7038ac
hebaayase <hebaheshamayase@gmail.com>
2026-07-01 02:15:15 +0300

    Add CreateCenter Controller

---
+++ Sehatak.API/Controllers/CenterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sehatak.Application.DTOs.CreateCenterRequestDto;
+using Sehatak.Application.Interfaces.MedicalCenter;
+using Sehatak.Infrastructure.Services.CenterService;
+
+namespace Sehatak.API.Controllers
+{
+    [ApiController]
+    [Route("api/admin/centers")]
+    public class CenterController : ControllerBase
+    {
+        private readonly ICenterService centerService;
+        public CenterController(ICenterService centerService)
+        {
+            this.centerService = centerService;
+        }
+
+        [Authorize(Policy = "SuperAdminOnly")]
+        [HttpPost("CreateCenter")]
+        public async Task<IActionResult> CreateCenter([FromBody]createCenterRequestDto request)
+        {
+            var result = await centerService.CreateCenterAsync(request);
+            return Ok(result);
+        }
+    }
+}
