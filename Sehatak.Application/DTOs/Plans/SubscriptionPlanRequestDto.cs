67ff3836e94c1b6c79a4093907b94e7258ac5a53
hebaayase <hebaheshamayase@gmail.com>
2026-06-30 19:23:42 +0300

    Add Subscription plan Dto

--- Sehatak.Application/DTOs/Plans/SubscriptionPlanRequestDto.cs
+++ Sehatak.Application/DTOs/Plans/SubscriptionPlanRequestDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,13 @@ namespace Sehatak.Application.DTOs.Plans
 {
     public class SubscriptionPlanRequestDto
     {
+        [Required]
         public string Name { get; set; } = string.Empty;
-
+        [Required]
         public decimal Price { get; set; }
-
+        [Required]
         public int DurationDays { get; set; }
-
+        [Required]
         public int platformFeatureId { get; set; }
     }
 }
