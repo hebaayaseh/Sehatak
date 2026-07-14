fa5f7c2857fe2c9830d7ea12863d381169408920
hebaayase <hebaheshamayase@gmail.com>
2026-07-14 19:28:43 +0300

    Get LapTechnical and Receptionist Conttroller

---
+++ Sehatak.API/Controllers/ForAllActors/GetReceptionistController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sehatak.Application.Interfaces.GetSttafInterFace;
+
+namespace Sehatak.API.Controllers.ForAllActors
+{
+    [ApiController]
+    [Route("get-Receptionist-and-Lap-Technical")]
+    public class GetReceptionistController : ControllerBase
+    {
+        private readonly IGetStaff getStaff;
+        public GetReceptionistController(IGetStaff getStaff)
+        {
+            this.getStaff = getStaff;
+        }
+
+        [HttpGet("Get-Receptionist/{centerId}")]
+        public async Task<IActionResult> GetReceptionistAsync(int centerId)
+        {
+            var result = await getStaff.GetReceptionistsAsync(centerId);
+            return Ok(result);
+        }
+
+        [HttpGet("Get-Lap-Tecnical/{centerId}")]
+        public async Task<IActionResult> GetLapTechnicalAsync(int centerId)
+        {
+            var result = await getStaff.GetLapTechnicalsAsync(centerId);
+            return Ok(result);
+        }
+    }
+}
