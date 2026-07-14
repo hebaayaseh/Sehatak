107c434359bdee1667c79ab15482bda7c9e315c6
hebaayase <hebaheshamayase@gmail.com>
2026-07-15 01:47:43 +0300

    Edit center information controller

---
+++ Sehatak.API/Controllers/SuperAdminAndAdmin/EditProfilecontroller/EditAdminProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sehatak.Application.DTOs.EditProfile.EditProfileActors;
+using Sehatak.Application.Interfaces.IProfileInterface.ProfileAdmin;
+
+namespace Sehatak.API.Controllers.SuperAdminAndAdmin.EditProfilecontroller
+{
+    [ApiController]
+    [Route("api-edit-center-admin-informations")]
+    public class EditAdminProfileController : ControllerBase
+    {
+        private readonly IprofileAdmin iprofile;
+        public EditAdminProfileController(IprofileAdmin iprofile)
+        {
+            this.iprofile = iprofile;
+        }
+
+        [Authorize(Policy = "AdminOnly")]
+        [HttpPost("edit-center-information/{centerId}")]
+        public async Task<IActionResult> EditCenterIngoration(int centerId , [FromForm] EditCenterInformationRequest request)
+        {
+            var result = await iprofile.EditCenterInformation(centerId, request);
+            return Ok(result);
+        }
+    }
+}
