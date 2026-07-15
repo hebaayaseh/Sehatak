2418c1996db672c6bb5bc84a0642f55a7223301c
hebaayase <hebaheshamayase@gmail.com>
2026-07-15 21:46:32 +0300

    add doctor daily hours work controller

---
+++ Sehatak.API/Controllers/SuperAdminAndAdmin/AddDoctorDailycontroller/AddDoctorDaiylController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sehatak.Application.DTOs.AddDoctorDailyHour;
+using Sehatak.Application.Interfaces.AddDoctorDailyHours;
+using System.Security.Claims;
+
+namespace Sehatak.API.Controllers.SuperAdminAndAdmin.AddDoctorDaiktcontroller
+{
+    [ApiController]
+    [Route("api-add-doctor-daily-hours")]
+    public class AddDoctorDaiylController : ControllerBase
+    {
+        private readonly IAddDoctorDailyHours addHours;
+        public AddDoctorDaiylController(IAddDoctorDailyHours addHours)
+        {
+            this.addHours = addHours;
+        }
+        [Authorize(Policy = "AdminOnly")]
+        [HttpPost("add-doctor-hours/{centerId}/{doctorId}")]
+        public async Task<IActionResult> AddDoctorDailyHours(int centerId , int doctorId,
+            [FromBody] AddDoctorDailyHoursRequest request)
+        {
+            var userId = int.Parse(
+                User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+
+            var result = await addHours.AddDoctorDailyHoursAsync(centerId,doctorId,userId,request);
+            return Ok(result);
+        }
+
+    }
+}
